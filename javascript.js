a945cb1a2e39bc883f25cf64a4019e0bc39b8a51
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2025-05-02 13:25:42 -0400

    Create playGame function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
+playGame();
+
 function getHumanChoice() {
-    const humanChoice = prompt("Rock, Paper, or Scissors? ");
+    const humanChoice = prompt("Rock, Paper, or Scissors?");
     return humanChoice;
 }
 
@@ -9,24 +11,32 @@ function getComputerChoice() {
     return choices[computerSelection];
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
 
-function playRound(humanChoice, computerChoice) {
-    //capitalize humanChoice if it is not already capitalized, as it will be used for formatting
-    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
-
-    const humanWonRound = (humanChoice === "Rock" && computerChoice === "Scissors")
-                       || (humanChoice === "Paper" && computerChoice === "Rock")
-                       || (humanChoice === "Scissors" && computerChoice === "Paper");
-
-    if(humanWonRound) {
-        alert(`You win this round! ${humanChoice} beats ${computerChoice}`);
-        humanScore++;
-    } else if(humanChoice === computerChoice) {
-        alert(`Draw! ${humanChoice} does not beat ${computerChoice}`);
-    } else {
-        alert(`You lose this round! ${computerChoice} beats ${humanChoice}`);
-        computerScore++
+    function playRound(humanChoice, computerChoice) {
+        //capitalize humanChoice if it is not already capitalized, as it will be used for formatting
+        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    
+        const humanWonRound = (humanChoice === "Rock" && computerChoice === "Scissors")
+                           || (humanChoice === "Paper" && computerChoice === "Rock")
+                           || (humanChoice === "Scissors" && computerChoice === "Paper");
+    
+        if(humanWonRound) {
+            alert(`You win this round! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        } else if(humanChoice === computerChoice) {
+            alert(`Draw! ${humanChoice} does not beat ${computerChoice}`);
+        } else {
+            alert(`You lose this round! ${computerChoice} beats ${humanChoice}`);
+            computerScore++
+        }
     }
 }
