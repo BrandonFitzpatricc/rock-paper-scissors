93baf9ce76e9473585bb73a98305fb929cedba82
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2025-05-02 12:06:18 -0400

    Create playRound function

--- javascript.js
+++ javascript.js
@@ -8,3 +8,25 @@ function getComputerChoice() {
     const computerSelection = Math.floor(Math.random() * 3);
     return choices[computerSelection];
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    //capitalize humanChoice if it is not already capitalized, as it will be used for formatting
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+
+    const humanWonRound = (humanChoice === "Rock" && computerChoice === "Scissors")
+                       || (humanChoice === "Paper" && computerChoice === "Rock")
+                       || (humanChoice === "Scissors" && computerChoice === "Paper");
+
+    if(humanWonRound) {
+        alert(`You win this round! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else if(humanChoice === computerChoice) {
+        alert(`Draw! ${humanChoice} does not beat ${computerChoice}`);
+    } else {
+        alert(`You lose this round! ${computerChoice} beats ${humanChoice}`);
+        computerScore++
+    }
+}
